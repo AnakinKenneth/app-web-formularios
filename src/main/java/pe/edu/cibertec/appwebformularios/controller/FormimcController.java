85bbfdf8199db2858248f8e106ebeced768ba803
Luis181996 <luishumberto181996@hotmail.com>
2023-09-24 10:56:46 -0500

    ejercicio 2 terminado

---
+++ src/main/java/pe/edu/cibertec/appwebformularios/controller/FormimcController.java
@@ -0,0 +1,48 @@
+package pe.edu.cibertec.appwebformularios.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import pe.edu.cibertec.appwebformularios.model.ImcModel;
+
+@Controller
+public class FormimcController {
+    @GetMapping("/calcularimc")
+    public String index(Model model){
+        model.addAttribute("imcmodel", new ImcModel());
+        model.addAttribute("verresultado",false);
+        return "formImc";
+    }
+    @PostMapping("/calcularimc")
+    public String calcularimc(@ModelAttribute("imcmodel") ImcModel imcmodel,
+                                                          Model model){
+        Double tallam = imcmodel.getAltura() / 100;
+        Double valorimc = imcmodel.getPeso() / (tallam * tallam);
+        String resultadoimc = "";
+        String coloralert ="alert-danger";
+        if(valorimc <=18.5){
+            resultadoimc = "Por denajo del peso.";
+            coloralert = "alert-dark";
+        } else  if(valorimc <=25){
+            resultadoimc = "Con peso normal.";
+            coloralert = "alert-primary";
+        } else  if(valorimc <=30){
+            resultadoimc = "Con sobrepeso.";
+            coloralert = "alert-warning";
+        } else  if(valorimc <=35){
+            resultadoimc = "Com obesidad leve.";
+        } else  if(valorimc <=39){
+            resultadoimc = "Con obesidadmedia.";
+        } else {
+            resultadoimc = "Con obesidad morbida.";
+        }
+        model.addAttribute("verresultado",true);
+        model.addAttribute("resultado","Su valor de IMC es:" +
+                String.format("%.2f", valorimc) +
+                " , usted se encuentra; "+ resultadoimc);
+        model.addAttribute("coloralert",coloralert);
+        return "formimc";
+    }
+}
